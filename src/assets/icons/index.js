c83d3a51a0f7e4df02ead88b8ed563a40eb16920
kevin_hws <kevinshu1995@gmail.com>
2021-11-21 23:09:41 +0800

    feat: 🎸 icons & logo

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,9 @@
-// import bicycle from "./bicycle.svg?raw";
+import bell from "./bell.svg?raw";
+import download from "./download.svg?raw";
+import close from "./close.svg?raw";
+import compass from "./compass.svg?raw";
+import home from "./home.svg?raw";
+import menu from "./menu.svg?raw";
+import wallet from "./wallet.svg?raw";
 
-export {};
+export { download, menu, bell, compass, home, wallet, close };
