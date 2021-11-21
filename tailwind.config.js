1e0aaf3f0bc02e9f18a5202555c893b02e78fff8
kevin_hws <kevinshu1995@gmail.com>
2021-11-21 23:08:52 +0800

    feat: 🎸 tailwind theme & google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
-// const colors = require('tailwindcss/colors')
-
 module.exports = {
-  mode: 'jit',
-  purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: "jit",
+    purge: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: "#F3E9D8",
+                secondary: "#EB7A77",
+                tertiary: "#5457A5",
+                paragraph: "#91989F",
+                highlight: "#EDBA55",
+                grey: {
+                    light: "#FCFAF2",
+                    dark: "#333333",
+                },
+                dark: "#262626",
+            },
+        },
+    },
+    plugins: [],
+};
