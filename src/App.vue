315614703294978e4b79afa7982140574c0b22b1
kevin_hws <kevinshu1995@gmail.com>
2021-11-21 17:28:39 +0800

    feat: 🎸 App grid view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="text-center">
-        <h1>hello</h1>
-        <router-view :key="$route.name" />
+    <div class="grid main-grid-template w-screen h-screen">
+        <header>header</header>
+        <main>
+            <router-view :key="$route.name" />
+        </main>
+        <footer class="hidden sm:block">footer</footer>
     </div>
 </template>
 
@@ -11,3 +14,31 @@ export default {
     setup() {},
 };
 </script>
+
+<style scoped>
+.main-grid-template {
+    grid-template:
+        "head" 64px
+        "main" 1fr
+        / 1fr;
+}
+@screen sm {
+    .main-grid-template {
+        grid-template:
+            "head" 64px
+            "main" 1fr
+            "footer" 64px
+            / 1fr;
+    }
+}
+
+header {
+    grid-area: head;
+}
+main {
+    grid-area: main;
+}
+footer {
+    grid-area: footer;
+}
+</style>
