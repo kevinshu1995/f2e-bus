7218398eb69171f88c8a9b487fd22cee483e72b8
kevin_hws <kevinshu1995@gmail.com>
2021-11-28 21:19:20 +0800

    feat: 🎸 new plugins and mixins

--- src/plugins/leaflet.js
+++ src/plugins/leaflet.js
@@ -1,7 +1,45 @@
 import "leaflet/dist/leaflet.css";
 import "leaflet";
 
-const defaultPosition = [25.0466152, 121.5159722];
+const defaultLocations = {
+    taipei: [25.0466152, 121.5159722],
+    zhongLi: [24.956063294889482, 121.21563434600831],
+    taiChung: [24.147703227978166, 120.67296981811525],
+};
+const defaultPosition = defaultLocations.taiChung;
+
+const leaflet = L;
+
+const iconOptions = {
+    iconSize: [32, 32], // size of the icon
+    shadowSize: [0, 0], // size of the shadow
+    iconAnchor: [16, 16], // point of the icon which will correspond to marker's location
+    shadowAnchor: [0, 0], // the same for the shadow
+    popupAnchor: [0, -24], // point from which the popup should open relative to the iconAnchor
+};
+
+const icons = {
+    default: L.icon({
+        iconUrl: "/mapMarker-default.svg",
+        ...iconOptions,
+    }),
+    outline: L.icon({
+        iconUrl: "/mapMarker-outline.svg",
+        ...iconOptions,
+    }),
+    active: L.icon({
+        iconUrl: "/mapMarker-active.svg",
+        ...iconOptions,
+    }),
+    self: L.icon({
+        iconUrl: "/mapMarker-currentPosition.svg",
+        iconSize: [22, 22], // size of the icon
+        shadowSize: [0, 0], // size of the shadow
+        iconAnchor: [11, 11], // point of the icon which will correspond to marker's location
+        shadowAnchor: [0, 0], // the same for the shadow
+        popupAnchor: [0, -19], // point from which the popup should open relative to the iconAnchor
+    }),
+};
 
 function initMap() {
     const map = L.map("map", {
@@ -30,4 +68,4 @@ function initMap() {
     return map;
 }
 
-export { initMap };
+export { initMap, leaflet, icons };
