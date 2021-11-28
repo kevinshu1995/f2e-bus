eb7f90b0c9ddf168fa7925645baa4ff76de046a3
kevin_hws <kevinshu1995@gmail.com>
2021-11-28 21:18:43 +0800

    feat: 🎸 new icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,7 +1,9 @@
 import bell from "./bell.svg?raw";
 import bookmark from "./bookmark.svg?raw";
+import clock from "./clock.svg?raw";
 import close from "./close.svg?raw";
 import compass from "./compass.svg?raw";
+import directionArrow from "./direction-arrow.svg?raw";
 import download from "./download.svg?raw";
 import flag from "./flag.svg?raw";
 import home from "./home.svg?raw";
@@ -11,4 +13,19 @@ import refresh from "./refresh.svg?raw";
 import search from "./search.svg?raw";
 import wallet from "./wallet.svg?raw";
 
-export { download, menu, bell, compass, home, wallet, close, info, search, bookmark, flag, refresh };
+export {
+    download,
+    menu,
+    bell,
+    compass,
+    home,
+    wallet,
+    close,
+    info,
+    search,
+    bookmark,
+    flag,
+    refresh,
+    clock,
+    directionArrow,
+};
